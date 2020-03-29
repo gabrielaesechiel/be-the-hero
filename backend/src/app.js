91c6c926ea06f3b256641cb5d45ef41d0005dd98
gabizidorio <gabrielaizidorio18@gmail.com>
2020-03-29 19:47:13 -0300

    Atualizando arquivos

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
+
 const routes = require('./routes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use(routes);
+app.use(errors());
+
+module.exports = app;
+
 
 /**
  * Rota / Recurso
@@ -39,4 +46,3 @@ app.use(routes);
   * Query Builder: table('users').select('*').where()
   */
 
-app.listen(3333);
